f639fc359686ce02d96f730af6bc502decba8f59
Suresh Parthasarathy <supartha@microsoft.com>
2017-10-12 17:21:37 +0530

    First version of Protonn test bat file

--- run_ProtoNN_usps10.bat
+++ run_ProtoNN_usps10.bat
@@ -1,72 +1,65 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT license.
+@ECHO OFF
+REM # Copyright (c) Microsoft Corporation. All rights reserved.
+REM # Licensed under the MIT license.
 
-# Have a look at README.md and README_PROTONN_OSS.md for details on how to setup the data directory to run this script.
-
-
-########################################################
-# Input-output parameters
-########################################################
-
-predefined_model="-P 0"
-problem_format="-C 1"
-input_dir="-I usps10"
-input_format="-F 0"
+REM # Have a look at README.md and README_PROTONN_OSS.md for details on how to setup the data directory to run this script.
 
 
+REM ########################################################
+REM # Input-output parameters
+REM ########################################################
 
-########################################################
-# Data-dependent parameters
-########################################################
+SET predefined_model=-P 0
+SET problem_format=-C 1
+SET input_dir=-I usps10
+SET input_format=-F 0
 
-ntrain="-r 7291"
-ntest="-e 2007"
-original_dimension="-D 256"
-num_labels="-l 10" 
+REM ########################################################
+REM # Data-dependent parameters
+REM ########################################################
 
+SET ntrain=-r 7291
+SET ntest=-e 2007
+SET original_dimension=-D 256
+SET num_labels=-l 10
 
+REM ########################################################
+REM # ProtoNN hyper-parameters (required)
+REM ########################################################
 
-########################################################
-# ProtoNN hyper-parameters (required)
-########################################################
-
-projection_dimension="-d 15"
-#prototypes="-k 20"
-prototypes="-m 200"
-
-
-
-########################################################
-# ProtoNN hyper-parameters (optional)
-########################################################
-
-lambda_W="-W 1.0"
-lambda_Z="-Z 1.0"
-lambda_B="-B 1.0"
-gammaNumerator="-g 1.0"
-normalization="-N 0"
-seed="-R 42"
+SET projection_dimension=-d 15
+REM #prototypes=-k 20
+SET prototypes=-m 200
 
 
+REM ########################################################
+REM # ProtoNN hyper-parameters (optional)
+REM ########################################################
 
-########################################################
-# ProtoNN optimization hyper-parameters (optional)
-########################################################
-
-batch_size="-b 1024"
-iters="-T 20"
-epochs="-E 20"
-
+SET lambda_W=-W 1.0
+SET lambda_Z=-Z 1.0
+SET lambda_B=-B 1.0
+SET gammaNumerator=-g 1.0
+SET normalization=-N 0
+SET seed=-R 42
 
 
-########################################################
-# execute ProtoNN
-########################################################
+REM ########################################################
+REM # ProtoNN optimization hyper-parameters (optional)
+REM ########################################################
 
-#gdb=" gdb --args" 
-executable="./build/bin/ProtoNN"
-command=$gdb" "$executable" "$predefined_model" "$seed" "$problem_format" "$input_dir" "$input_format" "$ntrain" "$ntest" "$original_dimension" "$projection_dimension" "$num_labels" "$prototypes" "$lambda_W" "$lambda_Z" "$lambda_B" "$gammaNumerator" "$batch_size" "$iters" "$epochs" "$normalization
-echo "Running ProtoNN with following command: "
-echo $command
-echo ""
-exec $command
+SET batch_size=-b 1024
+SET iters=-T 20
+SET epochs=-E 20
+
+REM ########################################################
+REM # execute ProtoNN
+REM ########################################################
+
+SET executable=./build/bin/ProtoNN
+SET command=%executable% %predefined_model% %seed% %problem_format% %input_dir% %input_format% %ntrain% %ntest% %original_dimension% %projection_dimension% %num_labels% %prototypes% %lambda_W% %lambda_Z% %lambda_B% %gammaNumerator% %batch_size% %iters% %epochs% %normalization%
+ECHO Running ProtoNN with following command: 
+ECHO $command
+ECHO 
+START %command%
+pause
