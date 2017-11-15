dcb9325051c5786f44f025878e45cd33540bf3eb
Aditya Kusupati <t-vekusu@microsoft.com>
2017-11-16 00:02:54 +0530

    Added shel lscripts for Train and Predict

---
+++ run_BonsaiPredict_usps10.sh
@@ -0,0 +1,31 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+# Have a look at README_BONSAI_OSS.md for details on how to setup the directory to run this script.
+
+
+########################################################
+# Input-output parameters
+########################################################
+
+input_dir="-D ./usps10"
+input_format="-f 0"
+model_dir="-M ./usps10/BonsaiResults/23_44_23_15_11" # Note: The model_dir has to be changed as Model naming is based on timestamp so required to be changed by the user
+
+########################################################
+# Data-dependent parameters
+########################################################
+
+ntest="-N 2007"
+
+########################################################
+# execute Bonsai
+########################################################
+
+#gdb=" gdb --args"
+executable="./BonsaiPredict"
+command=$gdb" "$executable" "$input_format" "$ntest" "$input_dir" "$model_dir
+echo "Running Bonsai with following command: "
+echo $command
+echo ""
+exec $command
