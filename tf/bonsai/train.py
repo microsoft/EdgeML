71135ec11ae661ba678c2dd262a69053bfbdfdde
Aditya Kusupati <t-vekusu@microsoft.com>
2018-02-14 02:23:09 +0530

    Cleaned up and reorganised Bonsai TF

--- tf/bonsai/train.py
+++ tf/bonsai/train.py
@@ -1,14 +1,14 @@
 import utils
 import tensorflow as tf
 import numpy as np
-import sys
+from bonsaiTrainer import BonsaiTrainer
 from bonsai import Bonsai
 
-## Fixing seeds for reproducibility
+# Fixing seeds for reproducibility
 tf.set_random_seed(42)
 np.random.seed(42)
 
-## Hyper Param pre-processing
+# Hyper Param pre-processing
 args = utils.getArgs()
 
 sigma = args.sigma
@@ -26,154 +26,56 @@ learningRate = args.learningRate
 
 data_dir = args.data_dir
 
-(dataDimension, numClasses, 
-	Xtrain, Ytrain, Xtest, Ytest) = utils.preProcessData(data_dir)
+(dataDimension, numClasses,
+    Xtrain, Ytrain, Xtest, Ytest) = utils.preProcessData(data_dir)
 
 sparZ = args.sZ
 
-if numClasses == 2:
-	sparW = 1
-	sparV = 1
-	sparT = 1
+if numClasses > 2:
+    sparW = 0.2
+    sparV = 0.2
+    sparT = 0.2
 else:
-	sparW = 0.2
-	sparV = 0.2
-	sparT = 0.2
+    sparW = 1
+    sparV = 1
+    sparT = 1
 
 if args.sW is not None:
-	sparW = args.sW
+    sparW = args.sW
 if args.sV is not None:
-	sparV = args.sV
+    sparV = args.sV
 if args.sT is not None:
-	sparT = args.sT
+    sparT = args.sT
 
 if args.batchSize is None:
-	batchSize = np.maximum(100, int(np.ceil(np.sqrt(Ytrain.shape[0]))))
+    batchSize = np.maximum(100, int(np.ceil(np.sqrt(Ytrain.shape[0]))))
 else:
-	batchSize = args.batchSize
+    batchSize = args.batchSize
 
+lossFlag = True
 
-## Creation of Bonsai Object
-bonsaiObj = Bonsai(numClasses, dataDimension, projectionDimension, depth, sigma, 
-	regW, regT, regV, regZ, sparW, sparT, sparV, sparZ, lr = learningRate)
+X = tf.placeholder("float32", [None, dataDimension])
+Y = tf.placeholder("float32", [None, numClasses])
+
+bonsaiObj = Bonsai(numClasses, dataDimension,
+                   projectionDimension, depth, sigma, X)
+
+bonsaiTrainer = BonsaiTrainer(bonsaiObj,
+                              regW, regT, regV, regZ,
+                              sparW, sparT, sparV, sparZ,
+                              learningRate, Y, lossFlag)
 
 sess = tf.InteractiveSession()
-sess.run(tf.group(tf.initialize_all_variables(), tf.initialize_variables(tf.local_variables())))
-saver = tf.train.Saver()   ## Use it incase of saving the model
+sess.run(tf.group(tf.initialize_all_variables(),
+                  tf.initialize_variables(tf.local_variables())))
+saver = tf.train.Saver()
 
-numIters = Xtrain.shape[0]/batchSize
+bonsaiTrainer.train(batchSize, totalEpochs, sess, Xtrain, Xtest, Ytrain, Ytest)
 
-totalBatches = numIters*totalEpochs
+print(bonsaiTrainer.bonsaiObj.score.eval(feed_dict={X: Xtest}))
+print(bonsaiObj.score.eval(feed_dict={X: Xtest}))
 
-counter = 0
-if bonsaiObj.numClasses > 2:
-	trimlevel = 15
-else:
-	trimlevel = 5
-ihtDone = 0
-
-
-for i in range(totalEpochs):
-	print("\nEpoch Number: "+str(i))
-
-	trainAcc = 0.0
-	for j in range(numIters):
-
-		if counter == 0:
-			print("\n******************** Dense Training Phase Started ********************\n")
-
-		## Updating the indicator sigma
-		if ((counter == 0) or (counter == int(totalBatches/3)) or (counter == int(2*totalBatches/3))):
-			bonsaiObj.sigmaI = 1
-			itersInPhase = 0
-
-		elif (itersInPhase%100 == 0):
-			indices = np.random.choice(Xtrain.shape[0],100)
-			batchX = Xtrain[indices,:]
-			batchY = Ytrain[indices,:]
-			batchY = np.reshape(batchY, [-1, bonsaiObj.numClasses])
-
-			_feed_dict = {bonsaiObj.x: batchX, bonsaiObj.y: batchY}
-			Xcapeval = bonsaiObj.Xeval.eval(feed_dict=_feed_dict)
-			Teval = bonsaiObj.Teval.eval()
-			
-			sum_tr = 0.0
-			for k in range(0, bonsaiObj.internalNodes):
-				sum_tr += (np.sum(np.abs(np.dot(Teval[k], Xcapeval))))
-
-			if(bonsaiObj.internalNodes > 0):
-				sum_tr /= (100*bonsaiObj.internalNodes)
-				sum_tr = 0.1/sum_tr
-			else:
-				sum_tr = 0.1
-			sum_tr = min(1000,sum_tr*(2**(float(itersInPhase)/(float(totalBatches)/30.0))))
-
-			bonsaiObj.sigmaI = sum_tr
-		
-		itersInPhase += 1
-		batchX = Xtrain[j*batchSize:(j+1)*batchSize]
-		batchY = Ytrain[j*batchSize:(j+1)*batchSize]
-		batchY = np.reshape(batchY, [-1, bonsaiObj.numClasses])
-
-		if bonsaiObj.numClasses > 2:
-			_feed_dict = {bonsaiObj.x: batchX, bonsaiObj.y: batchY, bonsaiObj.batch_th: batchY.shape[0]}
-		else:
-			_feed_dict = {bonsaiObj.x: batchX, bonsaiObj.y: batchY}
-
-		## Mini-batch training
-		batchLoss = bonsaiObj.runTraining(sess, _feed_dict)
-
-		batchAcc = bonsaiObj.accuracy.eval(feed_dict=_feed_dict)
-		trainAcc += batchAcc
-
-		## Training routine involving IHT and sparse retraining
-		if (counter >= int(totalBatches/3) and (counter < int(2*totalBatches/3)) and counter%trimlevel == 0):
-			bonsaiObj.runHardThrsd(sess)
-			if ihtDone == 0:
-				print("\n******************** IHT Phase Started ********************\n")
-			ihtDone = 1
-		elif ((ihtDone == 1 and counter >= int(totalBatches/3) and (counter < int(2*totalBatches/3)) 
-			and counter%trimlevel != 0) or (counter >= int(2*totalBatches/3))):
-			bonsaiObj.runSparseTraining(sess)
-			if counter == int(2*totalBatches/3):
-				print("\n******************** Sprase Retraining Phase Started ********************\n")
-		counter += 1
-
-	print("Train accuracy "+str(trainAcc/numIters)) 
-
-	if bonsaiObj.numClasses > 2:
-		_feed_dict = {bonsaiObj.x: Xtest, bonsaiObj.y: Ytest, bonsaiObj.batch_th: Ytest.shape[0]}
-	else:
-		_feed_dict = {bonsaiObj.x: Xtest, bonsaiObj.y: Ytest}
-
-	## this helps in direct testing instead of extracting the model out
-	oldSigmaI = bonsaiObj.sigmaI
-	bonsaiObj.sigmaI = 1e9
-
-	testAcc = bonsaiObj.accuracy.eval(feed_dict=_feed_dict)
-	if ihtDone == 0:
-		maxTestAcc = -10000
-		maxTestAccEpoch = i
-	else:
-		if maxTestAcc <= testAcc:
-			maxTestAccEpoch = i
-			maxTestAcc = testAcc
-
-	print("Test accuracy %g"%testAcc)
-
-	testLoss = bonsaiObj.loss.eval(feed_dict=_feed_dict)
-	regTestLoss = bonsaiObj.regLoss.eval(feed_dict=_feed_dict)
-	print("MarginLoss + RegLoss: " + str(testLoss - regTestLoss) + " + " + str(regTestLoss) + " = " + str(testLoss) + "\n")
-
-	bonsaiObj.sigmaI = oldSigmaI
-	sys.stdout.flush()
-
-print("Maximum Test accuracy at compressed model size(including early stopping): " 
-	+ str(maxTestAcc) + " at Epoch: " + str(maxTestAccEpoch) + "\nFinal Test Accuracy: " + str(testAcc))
-print("\nNon-Zeros: " + str(bonsaiObj.getModelSize()) + " Model Size: " + 
-	str(float(bonsaiObj.getModelSize())/1024.0) + " KB \n")
-
-np.save("W.npy", bonsaiObj.Weval.eval())
-np.save("V.npy", bonsaiObj.Veval.eval())
-np.save("Z.npy", bonsaiObj.Zeval.eval())
-np.save("T.npy", bonsaiObj.Teval.eval())
+np.save("W.npy", bonsaiTrainer.bonsaiObj.W.eval())
+np.save("V.npy", bonsaiTrainer.bonsaiObj.V.eval())
+np.save("Z.npy", bonsaiTrainer.bonsaiObj.Z.eval())
+np.save("T.npy", bonsaiTrainer.bonsaiObj.T.eval())
--- tf/bonsai/utils.py
+++ tf/bonsai/utils.py
@@ -2,138 +2,179 @@ import tensorflow as tf
 import numpy as np
 import argparse
 
-## Functions to check sanity of input arguments
+# Functions to check sanity of input arguments
+
+
 def checkIntPos(value):
-	ivalue = int(value)
-	if ivalue <= 0:
-		raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
-	return ivalue
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError(
+            "%s is an invalid positive int value" % value)
+    return ivalue
+
 
 def checkIntNneg(value):
-	ivalue = int(value)
-	if ivalue < 0:
-		raise argparse.ArgumentTypeError("%s is an invalid non-neg int value" % value)
-	return ivalue
+    ivalue = int(value)
+    if ivalue < 0:
+        raise argparse.ArgumentTypeError(
+            "%s is an invalid non-neg int value" % value)
+    return ivalue
+
 
 def checkFloatNneg(value):
-	fvalue = float(value)
-	if fvalue < 0:
-		raise argparse.ArgumentTypeError("%s is an invalid non-neg float value" % value)
-	return fvalue
+    fvalue = float(value)
+    if fvalue < 0:
+        raise argparse.ArgumentTypeError(
+            "%s is an invalid non-neg float value" % value)
+    return fvalue
+
 
 def checkFloatPos(value):
-	fvalue = float(value)
-	if fvalue <= 0:
-		raise argparse.ArgumentTypeError("%s is an invalid positive float value" % value)
-	return fvalue
+    fvalue = float(value)
+    if fvalue <= 0:
+        raise argparse.ArgumentTypeError(
+            "%s is an invalid positive float value" % value)
+    return fvalue
 
-## Function to parse the input arguments
+# Function to parse the input arguments
+
+
 def getArgs():
-	parser = argparse.ArgumentParser(description='HyperParams for Bonsai Algorithm')
-	parser.add_argument('-dir', '--data_dir', required=True, help='Data directory containing train.npy and test.npy')
-	
-	parser.add_argument('-d', '--depth', type=checkIntNneg, default=2, help='Depth of Bonsai Tree (default: 2 try: [0, 1, 3])')
-	parser.add_argument('-p', '--projDim', type=checkIntPos, default=10, help='Projection Dimension (default: 20 try: [5, 10, 30])')
-	parser.add_argument('-s', '--sigma', type=float, default=1.0, help='Parameter for sigmoid sharpness (default: 1.0 try: [3.0, 0.05, 0.1]')
-	parser.add_argument('-e', '--epochs', type=checkIntPos, default=42, help='Total Epochs (default: 42 try:[100, 150, 60])')
-	parser.add_argument('-b', '--batchSize', type=checkIntPos, help='Batch Size to be used (default: max(100, sqrt(train_samples)))')
-	parser.add_argument('-lr', '--learningRate', type=checkFloatPos, default=0.01, help='Initial Learning rate for Adam Oprimizer (default: 0.01)')
+    parser = argparse.ArgumentParser(
+        description='HyperParams for Bonsai Algorithm')
+    parser.add_argument('-dir', '--data_dir', required=True,
+                        help='Data directory containing train.npy and test.npy')
 
-	parser.add_argument('-rW', type=float, default=0.0001, help='Regularizer for predictor parameter W  (default: 0.0001 try: [0.01, 0.001, 0.00001])')
-	parser.add_argument('-rV', type=float, default=0.0001, help='Regularizer for predictor parameter V  (default: 0.0001 try: [0.01, 0.001, 0.00001])')
-	parser.add_argument('-rT', type=float, default=0.0001, help='Regularizer for branching parameter Theta  (default: 0.0001 try: [0.01, 0.001, 0.00001])')
-	parser.add_argument('-rZ', type=float, default=0.00001, help='Regularizer for projection parameter Z  (default: 0.00001 try: [0.001, 0.0001, 0.000001])')
+    parser.add_argument('-d', '--depth', type=checkIntNneg, default=2,
+                        help='Depth of Bonsai Tree (default: 2 try: [0, 1, 3])')
+    parser.add_argument('-p', '--projDim', type=checkIntPos, default=10,
+                        help='Projection Dimension (default: 20 try: [5, 10, 30])')
+    parser.add_argument('-s', '--sigma', type=float, default=1.0,
+                        help='Parameter for sigmoid sharpness (default: 1.0 try: [3.0, 0.05, 0.1]')
+    parser.add_argument('-e', '--epochs', type=checkIntPos, default=42,
+                        help='Total Epochs (default: 42 try:[100, 150, 60])')
+    parser.add_argument('-b', '--batchSize', type=checkIntPos,
+                        help='Batch Size to be used (default: max(100, sqrt(train_samples)))')
+    parser.add_argument('-lr', '--learningRate', type=checkFloatPos, default=0.01,
+                        help='Initial Learning rate for Adam Oprimizer (default: 0.01)')
 
-	parser.add_argument('-sW', type=checkFloatPos, help='Sparsity for predictor parameter W  (default: For Binary classification 1.0 else 0.2 try: [0.1, 0.3, 0.5])')
-	parser.add_argument('-sV', type=checkFloatPos, help='Sparsity for predictor parameter V  (default: For Binary classification 1.0 else 0.2 try: [0.1, 0.3, 0.5])')
-	parser.add_argument('-sT', type=checkFloatPos, help='Sparsity for branching parameter Theta  (default: For Binary classification 1.0 else 0.2 try: [0.1, 0.3, 0.5])')
-	parser.add_argument('-sZ', type=checkFloatPos, default=0.2, help='Sparsity for projection parameter Z  (default: 0.2 try: [0.1, 0.3, 0.5])')	
+    parser.add_argument('-rW', type=float, default=0.0001,
+                        help='Regularizer for predictor parameter W  (default: 0.0001 try: [0.01, 0.001, 0.00001])')
+    parser.add_argument('-rV', type=float, default=0.0001,
+                        help='Regularizer for predictor parameter V  (default: 0.0001 try: [0.01, 0.001, 0.00001])')
+    parser.add_argument('-rT', type=float, default=0.0001,
+                        help='Regularizer for branching parameter Theta  (default: 0.0001 try: [0.01, 0.001, 0.00001])')
+    parser.add_argument('-rZ', type=float, default=0.00001,
+                        help='Regularizer for projection parameter Z  (default: 0.00001 try: [0.001, 0.0001, 0.000001])')
 
-	return parser.parse_args()
+    parser.add_argument('-sW', type=checkFloatPos,
+                        help='Sparsity for predictor parameter W  (default: For Binary classification 1.0 else 0.2 try: [0.1, 0.3, 0.5])')
+    parser.add_argument('-sV', type=checkFloatPos,
+                        help='Sparsity for predictor parameter V  (default: For Binary classification 1.0 else 0.2 try: [0.1, 0.3, 0.5])')
+    parser.add_argument('-sT', type=checkFloatPos,
+                        help='Sparsity for branching parameter Theta  (default: For Binary classification 1.0 else 0.2 try: [0.1, 0.3, 0.5])')
+    parser.add_argument('-sZ', type=checkFloatPos, default=0.2,
+                        help='Sparsity for projection parameter Z  (default: 0.2 try: [0.1, 0.3, 0.5])')
 
-## Function for Multi Class Hinge Loss : TF has no internal implementation
+    return parser.parse_args()
+
+# Function for Multi Class Hinge Loss : TF has no internal implementation
+
+
 def multiClassHingeLoss(logits, label, batch_th):
-	flatLogits = tf.reshape(logits, [-1,])
-	correctId = tf.range(0, batch_th) * logits.shape[1] + label
-	correctLogit = tf.gather(flatLogits, correctId)
+    flatLogits = tf.reshape(logits, [-1, ])
+    correctId = tf.range(0, batch_th) * logits.shape[1] + label
+    correctLogit = tf.gather(flatLogits, correctId)
 
-	maxLabel = tf.argmax(logits, 1)
-	top2, _ = tf.nn.top_k(logits, k=2, sorted=True)
+    maxLabel = tf.argmax(logits, 1)
+    top2, _ = tf.nn.top_k(logits, k=2, sorted=True)
 
-	wrongMaxLogit = tf.where(tf.equal(maxLabel, label), top2[:,1], top2[:,0])
+    wrongMaxLogit = tf.where(tf.equal(maxLabel, label), top2[:, 1], top2[:, 0])
 
-	return tf.reduce_mean(tf.nn.relu(1. + wrongMaxLogit - correctLogit))
+    return tf.reduce_mean(tf.nn.relu(1. + wrongMaxLogit - correctLogit))
 
-## Function for cross entropy loss in multiclass case (for faster convergence in joint training)
+# Function for cross entropy loss in multiclass case (for faster convergence in joint training)
+
+
 def crossEntropyLoss(logits, label):
-	return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logit, labels=label))
+    return tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=label))
 
-## Hard Thresholding Function
+# Hard Thresholding Function
+
+
 def hardThreshold(A, s):
-	A_ = np.copy(A)
-	A_ = A_.ravel()
-	if len(A_) > 0:
-		th = np.percentile(np.abs(A_), (1 - s)*100.0, interpolation='higher')
-		A_[np.abs(A_)<th] = 0.0
-	A_ = A_.reshape(A.shape)
-	return A_
+    A_ = np.copy(A)
+    A_ = A_.ravel()
+    if len(A_) > 0:
+        th = np.percentile(np.abs(A_), (1 - s) * 100.0, interpolation='higher')
+        A_[np.abs(A_) < th] = 0.0
+    A_ = A_.reshape(A.shape)
+    return A_
 
-## Copy the support of src onto dest tensor
+# Copy the support of src onto dest tensor
+
+
 def copySupport(src, dest):
-	support = np.nonzero(src)
-	dest_ = dest
-	dest = np.zeros(dest_.shape)
-	dest[support] = dest_[support]
-	return dest
+    support = np.nonzero(src)
+    dest_ = dest
+    dest = np.zeros(dest_.shape)
+    dest[support] = dest_[support]
+    return dest
 
-## Function to read and pre-process data
+# Function to read and pre-process data
+
+
 def preProcessData(data_dir):
-	train = np.load(data_dir + '/train.npy')
-	test = np.load(data_dir + '/test.npy')
+    train = np.load(data_dir + '/train.npy')
+    test = np.load(data_dir + '/test.npy')
 
-	dataDimension = int(train.shape[1]) - 1
+    dataDimension = int(train.shape[1]) - 1
 
-	Xtrain = train[:, 1:dataDimension+1]
-	Ytrain_ = train[:, 0]
-	numClasses = max(Ytrain_) - min(Ytrain_) + 1
+    Xtrain = train[:, 1:dataDimension + 1]
+    Ytrain_ = train[:, 0]
+    numClasses = max(Ytrain_) - min(Ytrain_) + 1
 
-	Xtest = test[:,1:dataDimension+1]
-	Ytest_ = test[:,0]
+    Xtest = test[:, 1:dataDimension + 1]
+    Ytest_ = test[:, 0]
 
-	numClasses = int(max(numClasses, max(Ytest_) - min(Ytest_) + 1))
+    numClasses = int(max(numClasses, max(Ytest_) - min(Ytest_) + 1))
 
-	# Mean Var Normalisation
-	mean = np.mean(Xtrain,0)
-	std = np.std(Xtrain,0)
-	std[std[:]<0.000001] = 1
-	Xtrain = (Xtrain-mean)/std
+    # Mean Var Normalisation
+    mean = np.mean(Xtrain, 0)
+    std = np.std(Xtrain, 0)
+    std[std[:] < 0.000001] = 1
+    Xtrain = (Xtrain - mean) / std
 
-	Xtest = (Xtest-mean)/std
-	# End Mean Var normalisation
+    Xtest = (Xtest - mean) / std
+    # End Mean Var normalisation
 
-	lab = Ytrain_.astype('uint8')
-	lab = np.array(lab) - min(lab)
+    lab = Ytrain_.astype('uint8')
+    lab = np.array(lab) - min(lab)
 
-	lab_ = np.zeros((Xtrain.shape[0], numClasses))
-	lab_[np.arange(Xtrain.shape[0]), lab] = 1
-	if (numClasses == 2):
-		Ytrain = np.reshape(lab, [-1, 1])
-	else:
-		Ytrain = lab_
+    lab_ = np.zeros((Xtrain.shape[0], numClasses))
+    lab_[np.arange(Xtrain.shape[0]), lab] = 1
+    if (numClasses == 2):
+        Ytrain = np.reshape(lab, [-1, 1])
+    else:
+        Ytrain = lab_
 
-	lab = Ytest_.astype('uint8')
-	lab = np.array(lab)-min(lab)
+    lab = Ytest_.astype('uint8')
+    lab = np.array(lab) - min(lab)
 
-	lab_ = np.zeros((Xtest.shape[0], numClasses))
-	lab_[np.arange(Xtest.shape[0]), lab] = 1
-	if (numClasses == 2):
-		Ytest = np.reshape(lab, [-1, 1])
-	else:
-		Ytest = lab_
+    lab_ = np.zeros((Xtest.shape[0], numClasses))
+    lab_[np.arange(Xtest.shape[0]), lab] = 1
+    if (numClasses == 2):
+        Ytest = np.reshape(lab, [-1, 1])
+    else:
+        Ytest = lab_
 
-	trainBias = np.ones([Xtrain.shape[0], 1]);
-	Xtrain = np.append(Xtrain, trainBias, axis=1)
-	testBias = np.ones([Xtest.shape[0], 1]);
-	Xtest = np.append(Xtest, testBias, axis=1)
+    trainBias = np.ones([Xtrain.shape[0], 1])
+    Xtrain = np.append(Xtrain, trainBias, axis=1)
+    testBias = np.ones([Xtest.shape[0], 1])
+    Xtest = np.append(Xtest, testBias, axis=1)
 
-	return dataDimension, numClasses, Xtrain, Ytrain, Xtest, Ytest
+    if numClasses == 2:
+        numClasses = 1
+
+    return dataDimension + 1, numClasses, Xtrain, Ytrain, Xtest, Ytest
