72c0275d3b57bab3d127fbdfaef097c7a108daca
Don Dennis <donkdennis@gmail.com>
2018-02-19 19:57:40 +0530

    protoNN example initial commit

---
+++ protoNN_example.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, '../')
+import numpy as np
+import tensorflow as tf
+from edgeml.trainer.protoNNTrainer import ProtoNNTrainer
+from edgeml.graph.protoNN import ProtoNN
