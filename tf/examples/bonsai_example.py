4cc057c763b207f508c8489474a086f3d2a8ee90
Aditya Kusupati <t-vekusu@microsoft.com>
2018-07-05 15:20:02 +0530

    python2 and python3 sync up

--- tf/examples/bonsai_example.py
+++ tf/examples/bonsai_example.py
@@ -95,8 +95,14 @@ bonsaiTrainer.train(batchSize, totalEpochs, sess,
 # Non-Zeros: 24231.0 Model Size: 115.65625 KB hasSparse: True
 
 # Data - usps2
-# python bonsai_example.py -dir usps2/ -d 2 -p 22 -rW 0.00001 -rZ 0.0000001 -rV 0.00001 -rT 0.000001 -sZ 0.4 -sW 0.5 -sV 0.5 -sT 1 -e 300 -s 0.1 -b 20
+# python2 bonsai_example.py -dir usps2/ -d 2 -p 22 -rW 0.00001 -rZ 0.0000001 -rV 0.00001 -rT 0.000001 -sZ 0.4 -sW 0.5 -sV 0.5 -sT 1 -e 300 -s 0.1 -b 20
 # Maximum Test accuracy at compressed model size(including early stopping): 0.95715 at Epoch: 299
 # Final Test Accuracy: 0.952167
+
+# Non-Zeros: 2636.0 Model Size: 19.1328125 KB hasSparse: True
+
+# python3 bonsai_example.py -dir usps2/ -d 2 -p 22 -rW 0.00001 -rZ 0.0000001 -rV 0.00001 -rT 0.000001 -sZ 0.4 -sW 0.5 -sV 0.5 -sT 1 -e 300 -s 0.1 -b 20
+# Maximum Test accuracy at compressed model size(including early stopping): 0.96113604 at Epoch: 163
+# Final Test Accuracy: 0.94967616
 
 # Non-Zeros: 2636.0 Model Size: 19.1328125 KB hasSparse: True
