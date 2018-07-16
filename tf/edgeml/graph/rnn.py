58251c75461946fc10e4ecac5e422760d5dd6ec2
metastableB <donkdennis@gmail.com>
2018-07-16 09:51:19 +0000

    Started split into data -> forward -> train: added input without restore

--- tf/edgeml/graph/rnn.py
+++ tf/edgeml/graph/rnn.py
@@ -4,6 +4,83 @@
 import numpy as np
 import tensorflow as tf
 
+class EMI_DataPipeline():
+    '''The datainput block for EMI-RNN training
+    '''
+    def __init__(self, numSubinstance, numTimesteps, numFeats, numOutput,
+                 graph=None, prefetchNum =5):
+
+        self.numSubinstance = numSubinstance
+        self.numTimesteps = numTimesteps
+        self.numFeats = numFeats
+        self.graph = graph
+        self.prefetchNum = prefetchNum
+        self.numOutput = numOutput
+        self.graphCreated = False
+        # Either restore or create the following
+        self.X = None
+        self.Y = None
+        self.batchSize = None
+        self.numEpochs = None
+        self.dataset_init = None
+        self.dataset_next = None
+
+    def __createGraph(self):
+        assert self.graphCreated is False
+        dim = [None, self.numSubinstance, self.numTimesteps, self.numFeats]
+        X = tf.placeholder(tf.float32, dim, name='EMI/inpX')
+        Y = tf.placeholder(tf.float32, [None, self.numSubinstance,
+                                       self.numOutput], name='EMI/inpY')
+        batchSize = tf.placeholder(tf.int64, name='EMI/batch-size')
+        numEpochs = tf.placeholder(tf.int64, name='EMI/num-epochs')
+
+        dataset_x_target = tf.data.Dataset.from_tensor_slices(X)
+        dataset_y_target = tf.data.Dataset.from_tensor_slices(Y)
+        couple = (dataset_x_target, dataset_y_target)
+        ds_target = tf.data.Dataset.zip(couple).repeat(numEpochs)
+        ds_target = ds_target.batch(batchSize)
+        ds_target = ds_target.prefetch(self.prefetchNum)
+        ds_iterator_target = tf.data.Iterator.from_structure(ds_target.output_types,
+                                                    ds_target.output_shapes)
+        ds_next_target = ds_iterator_target.get_next()
+        ds_init_target = ds_iterator_target.make_initializer(ds_target,
+                                                             name='EMI/dataset-init')
+        self.X = X
+        self.Y = Y
+        self.batchSize = batchSize
+        self.numEpochs = numEpochs
+        self.dataset_init = ds_init_target
+        self.dataset_next = ds_next_target
+        self.graphCreated = True
+
+    def __call__(self):
+        if self.graphCreated is True:
+            return self.datset_next
+        if self.graph is None:
+            self.__createGraph()
+        else:
+            self.__restoreGraph()
+        return self.dataset_next
+
+    def runInitializer(self, sess, x_data, y_data, batchSize, numEpochs):
+        assert self.graphCreated is True
+        assert x_data.ndim == 4
+        assert x_data.shape[1] == self.numSubinstance
+        assert x_data.shape[2] == self.numTimesteps
+        assert x_data.shape[2] == self.numFeats
+        assert y_data.shape[0] == x_data.shape[0]
+        assert y_data.shape[1] == self.numSubinstance
+        assert y_data.shape[2] == self.numOutput
+        feed_dict = {
+            self.X: x_data,
+            self.Y: y_data,
+            self.batchSize: batchSize,
+            self.numEpochs: numEpochs
+        }
+        assert self.dataset_init is not None, 'Internal error!'
+        sess.run(self.dataset_init, feed_dict=feed_dict)
+
+
 class EMI_RNN():
     """Abstract base class for RNN architectures compatible with EMI-RNN. This
     class is extended by specific architectures like LSTM/GRU/FastGRNN etc.
