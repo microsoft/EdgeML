28124aa4704cc6c39724bc3a029c9266e0c4be34
Aditya Kusupati <t-vekusu@microsoft.com>
2018-08-14 20:00:36 +0530

    moving to main calls

--- tf/examples/Bonsai/bonsai_example.py
+++ tf/examples/Bonsai/bonsai_example.py
@@ -11,84 +11,89 @@ from edgeml.trainer.bonsaiTrainer import BonsaiTrainer
 from edgeml.graph.bonsai import Bonsai
 
 
-# Fixing seeds for reproducibility
-tf.set_random_seed(42)
-np.random.seed(42)
+def main():
+    # Fixing seeds for reproducibility
+    tf.set_random_seed(42)
+    np.random.seed(42)
 
-# Hyper Param pre-processing
-args = helpermethods.getArgs()
+    # Hyper Param pre-processing
+    args = helpermethods.getArgs()
 
-sigma = args.sigma
-depth = args.depth
+    sigma = args.sigma
+    depth = args.depth
 
-projectionDimension = args.proj_dim
-regZ = args.rZ
-regT = args.rT
-regW = args.rW
-regV = args.rV
+    projectionDimension = args.proj_dim
+    regZ = args.rZ
+    regT = args.rT
+    regW = args.rW
+    regV = args.rV
 
-totalEpochs = args.epochs
+    totalEpochs = args.epochs
 
-learningRate = args.learning_rate
+    learningRate = args.learning_rate
 
-dataDir = args.data_dir
+    dataDir = args.data_dir
 
-outFile = args.output_file
+    outFile = args.output_file
 
-(dataDimension, numClasses,
-    Xtrain, Ytrain, Xtest, Ytest) = helpermethods.preProcessData(dataDir)
+    (dataDimension, numClasses,
+        Xtrain, Ytrain, Xtest, Ytest) = helpermethods.preProcessData(dataDir)
 
-sparZ = args.sZ
+    sparZ = args.sZ
 
-if numClasses > 2:
-    sparW = 0.2
-    sparV = 0.2
-    sparT = 0.2
-else:
-    sparW = 1
-    sparV = 1
-    sparT = 1
+    if numClasses > 2:
+        sparW = 0.2
+        sparV = 0.2
+        sparT = 0.2
+    else:
+        sparW = 1
+        sparV = 1
+        sparT = 1
 
-if args.sW is not None:
-    sparW = args.sW
-if args.sV is not None:
-    sparV = args.sV
-if args.sT is not None:
-    sparT = args.sT
+    if args.sW is not None:
+        sparW = args.sW
+    if args.sV is not None:
+        sparV = args.sV
+    if args.sT is not None:
+        sparT = args.sT
 
-if args.batch_size is None:
-    batchSize = np.maximum(100, int(np.ceil(np.sqrt(Ytrain.shape[0]))))
-else:
-    batchSize = args.batch_size
+    if args.batch_size is None:
+        batchSize = np.maximum(100, int(np.ceil(np.sqrt(Ytrain.shape[0]))))
+    else:
+        batchSize = args.batch_size
 
-useMCHLoss = True
+    useMCHLoss = True
 
-if numClasses == 2:
-    numClasses = 1
+    if numClasses == 2:
+        numClasses = 1
 
-X = tf.placeholder("float32", [None, dataDimension])
-Y = tf.placeholder("float32", [None, numClasses])
+    X = tf.placeholder("float32", [None, dataDimension])
+    Y = tf.placeholder("float32", [None, numClasses])
 
-currDir = helpermethods.createTimeStampDir(dataDir)
+    currDir = helpermethods.createTimeStampDir(dataDir)
 
-helpermethods.dumpCommand(sys.argv, currDir)
+    helpermethods.dumpCommand(sys.argv, currDir)
 
-# numClasses = 1 for binary case
-bonsaiObj = Bonsai(numClasses, dataDimension,
-                   projectionDimension, depth, sigma)
+    # numClasses = 1 for binary case
+    bonsaiObj = Bonsai(numClasses, dataDimension,
+                       projectionDimension, depth, sigma)
 
-bonsaiTrainer = BonsaiTrainer(bonsaiObj,
-                              regW, regT, regV, regZ,
-                              sparW, sparT, sparV, sparZ,
-                              learningRate, X, Y, useMCHLoss, outFile)
+    bonsaiTrainer = BonsaiTrainer(bonsaiObj,
+                                  regW, regT, regV, regZ,
+                                  sparW, sparT, sparV, sparZ,
+                                  learningRate, X, Y, useMCHLoss, outFile)
 
-sess = tf.InteractiveSession()
-sess.run(tf.global_variables_initializer())
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
 
-saver = tf.train.Saver()
+    saver = tf.train.Saver()
 
-bonsaiTrainer.train(batchSize, totalEpochs, sess,
-                    Xtrain, Xtest, Ytrain, Ytest, dataDir, currDir)
+    bonsaiTrainer.train(batchSize, totalEpochs, sess,
+                        Xtrain, Xtest, Ytrain, Ytest, dataDir, currDir)
+
+
+if __name__ == '__main__':
+    main()
 
 # For the following command:
 # Data - Curet
