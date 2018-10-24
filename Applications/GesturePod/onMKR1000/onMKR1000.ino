ed7c61f69ee2f57c48117a0662b4247f9c1c6072
Shishir Patil <30296397+ShishirPatil@users.noreply.github.com>
2018-10-24 20:13:48 +0530

    Including license for I2Cdevlib in onMKR1000.ino (#66)
    * Including license for I2Cdevlib in onMKR1000.ino
    
    * Update onMKR1000.ino

--- Applications/GesturePod/onMKR1000/onMKR1000.ino
+++ Applications/GesturePod/onMKR1000/onMKR1000.ino
@@ -12,11 +12,34 @@
  * 1                       TX
  * 
  * Connections to MPU6050 using I2C protocol.
- * 
- * Thanks to Jeff Rowberg <jeff@rowberg.net> for his I2Cdev, 
- * MPU6050 and MPU6050_6Axis_MotionApps20 libraries
+ */
+
+/*
+ * Parts of this code are borrowed from Jeff Rowberg's I2Cdev device library.
+ * Below are the terms of license for I2Cdev device library:
+ */
+
+/*
+ * I2Cdev device library code is placed under the MIT license
+ * Copyright (c) 2012 Jeff Rowberg
  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 #include "config.h"
