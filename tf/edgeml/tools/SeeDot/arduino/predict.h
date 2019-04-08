4042af4f991f40ff0f10c1e8d2e6f93163cf855c
Sridhar Gopinath <g.sridhar53@gmail.com>
2019-04-08 12:13:36 +0530

    Temp removing files

--- tf/edgeml/tools/SeeDot/arduino/predict.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-#pragma once
-
-int predict();
-MYINT getIntFeature(MYINT);
-float getFloatFeature(MYINT);
