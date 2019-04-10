302812881df3a8a6977522ab9c28f83539bf3777
Don Dennis <donkdennis@gmail.com>
2019-04-10 08:12:03 +0000

    Stash commit

--- tf/setup.py
+++ tf/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='edgeml',
-    version='0.1',
+    version='0.2',
     packages=['edgeml', ],
     license='MIT License',
     long_description=open('../License.txt').read(),
