36411e14089b0a24ee62525f6466e6d0f2eb0429
Don Dennis <donkdennis@gmail.com>
2019-07-18 15:28:30 +0530

    Removing old Wakeword folder

--- Applications/WakeWord-MXChip/src/lib/algorithms/fc.c
+++
@@ -1,9 +0,0 @@
-#include "fc.h"
-
-void FCInference(const struct FCParams* fcParams, const float x[],
-	float* result, unsigned nonLinearity){
-	matrixVectorMul(fcParams->W, fcParams->outputDim,
-		fcParams->inputDim, x, result);
-	vectorVectorAdd(result, fcParams->B, fcParams->outputDim);
-}
-
