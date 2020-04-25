5f8465efb52f50a98647909f58eaa828da458a13
Harsha Vardhan Simhadri <harshasi@microsoft.com>
2020-04-24 23:23:53 -0700

    I/O trace for rnn pool block

--- c_reference/include/classifier.h
+++ c_reference/include/classifier.h
@@ -4,13 +4,8 @@
 #ifndef __CLASSIFIER_H__
 #define __CLASSIFIER_H__
 
-#include "utils.h"
-
-void FC(const float *const FCweights, const float *const FCbias,
-  const float *const input, const unsigned inputLen,
-  float* const classScores, const unsigned numClasses) {
-  matVec(FCweights, input, numClasses, inputLen, 0.0f, 1.0f, classScores);
-  v_add(1.0f, FCbias, 1.0f, classScores, numClasses, classScores);
-}
+void FC(const float* const FCweights, const float* const FCbias,
+  const float* const input, const unsigned inputLen,
+  float* const classScores, const unsigned numClasses);
 
 #endif
---
+++ c_reference/tests/rnnpool/test_rnnpool.c
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+#include <stdio.h>
+#include <string.h>
+
+#include "utils.h"
+#include "fastgrnn.h"
+#include "rnnpool.h"
+
+#include "traces/trace_0_0_input.h"
+#include "traces/trace_0_0_output.h"
+
+#include "vww_model/rnn1.h"
+#include "vww_model/rnn2.h"
+
+int main() {
+  FastGRNN_Params rnn1_params = {
+    .mean = mean1,
+    .stdDev = stdDev1,
+    .W = W1,
+    .U = U1,
+    .Bg = Bg1,
+    .Bh = Bh1,
+    .zeta = zeta1,
+    .nu = nu1
+  };
+
+  FastGRNN_Params rnn2_params = {
+    .mean = mean2,
+    .stdDev = stdDev2,
+    .W = W2,
+    .U = U2,
+    .Bg = Bg2,
+    .Bh = Bh2,
+    .zeta = zeta2,
+    .nu = nu2
+  };
+
+  float preComp1[HIDDEN_DIMS1] = { 0.0f };
+  float normFeatures1[INPUT_DIMS] = { 0.0f };
+  FastGRNN_LR_Buffers rnn1_buffers = {
+    .preComp = preComp1,
+    .normFeatures = normFeatures1
+  };
+
+  float preComp2[HIDDEN_DIMS2] = { 0.0f };
+  float normFeatures2[HIDDEN_DIMS1] = { 0.0f };
+  FastGRNN_LR_Buffers rnn2_buffers = {
+    .preComp = preComp2,
+    .normFeatures = normFeatures2
+  };
+
+  float output_test[4 * HIDDEN_DIMS2] = { 0.0f };
+  float buffer[HIDDEN_DIMS1 * PATCH_DIM];
+
+  rnnpool_block(input, INPUT_DIMS, PATCH_DIM, PATCH_DIM,
+    fastgrnn, HIDDEN_DIMS1, (const void*)(&rnn1_params), (void*)(&rnn1_buffers),
+    fastgrnn, HIDDEN_DIMS2, (const void*)(&rnn2_params), (void*)(&rnn2_buffers),
+    output_test, buffer);
+
+  float error = 0.0f;
+  for (unsigned d = 0; d < 4 * HIDDEN_DIMS2; ++d)
+    error += (output[d] - output_test[d]) * (output[d] - output_test[d]);
+  printf("Error: %f\n", error);
+}
