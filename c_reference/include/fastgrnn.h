8f6f4803da3afac0ab4b3acb7d69dd3c3a05e4e5
Harsha Vardhan Simhadri <harsha-simhadri@users.noreply.github.com>
2020-04-27 03:02:25 -0700

    C reference (#169)
    added rnn pool code and test case

--- c_reference/include/fastgrnn.h
+++ c_reference/include/fastgrnn.h
@@ -4,8 +4,6 @@
 #ifndef __FASTGRNN_H__
 #define __FASRGRNN_H__
 
-#include "utils.h"
-
 #define ERR_PRECOMP_NOT_INIT -1
 #define ERR_TEMPLRW_NOT_INIT -2
 #define ERR_TEMPLRU_NOT_INIT -3
@@ -64,48 +62,17 @@ typedef struct FastGRNN_LR_Buffers {
  * @param[in]       steps        number of steps of FastGRNN cell
  * @param[in]       params       pointer to model parameter
  * @param[in]       buffers      pointer to buffer spaces
+ * @param[in]       backward     direction of the pass, 0 for forward, 1 for backward
+ * @param[in]       normalize    apply mean-var normalization, 0 for no, 1 for yes
  * @return     The function returns <code>0</code> on success
  *             <code>ERR_PRECOMP_NOT_INIT</code> if preComp not allocated
  *             <code>ERR_TEMPLRW_NOT_INIT</code> if tempLRW not allocated
  *             <code>ERR_TEMPLRU_NOT_INIT</code> if tempLRU not allocated
  *             <code>ERR_NORMFEAT_NOT_INIT</code> if normFeatures not allocated
 */
-int fastgrnn_lr (float* const hiddenState, unsigned hiddenDims,
+int fastgrnn_lr(float* const hiddenState, unsigned hiddenDims,
   const float* const input, unsigned inputDims, unsigned steps,
-  const FastGRNN_LR_Params* params, FastGRNN_LR_Buffers * buffers) {
-
-  if (buffers->preComp == 0) return ERR_PRECOMP_NOT_INIT;
-  if (buffers->tempLRW == 0) return ERR_TEMPLRW_NOT_INIT;
-  if (buffers->tempLRU == 0) return ERR_TEMPLRU_NOT_INIT;
-  if (buffers->normFeatures == 0) return ERR_NORMFEATURES_NOT_INIT;
-
-  // #steps iterations of the RNN cell starting from hiddenState
-  for (unsigned t = 0; t < steps; t++) {
-    // Normalize the features
-    v_add(1.0f, input + t * inputDims, -1.0f, params->mean + t * inputDims,
-      inputDims, buffers->normFeatures);
-    v_div(params->stdDev + t * inputDims, buffers->normFeatures, inputDims, 
-      buffers->normFeatures);
-
-    // Process the new input and previous hidden state
-    matVec(params->W1, buffers->normFeatures, params->wRank, inputDims,
-      0.0f, 1.0f, buffers->tempLRW);
-    matVec(params->W2, buffers->tempLRW, hiddenDims, params->wRank, 
-      0.0f, 1.0f, buffers->preComp);
-    matVec(params->U1, hiddenState, params->uRank, hiddenDims, 
-      0.0f, 1.0f, buffers->tempLRU);
-    matVec(params->U2, buffers->tempLRU, hiddenDims, params->uRank, 
-      1.0f, 1.0f, buffers->preComp);
-
-    // Apply the gate to generate the new hidden state
-    for (unsigned i = 0; i < hiddenDims; i++) {
-      float gate = sigmoid(buffers->preComp[i] + params->Bg[i]);
-      float update = tanh(buffers->preComp[i] + params->Bh[i]);
-      hiddenState[i] = gate * hiddenState[i] + (params->zeta * (1.0 - gate) + params->nu) * update;
-    }
-  }
-  return 0;
-}
+  const void* params, void* buffers, int backward, int normalize);
 
 /**
  * @brief Model paramters for low-rank FastGRNN
@@ -148,38 +115,14 @@ typedef struct FastGRNN_Buffers {
  * @param[in]       steps        number of steps of FastGRNN cell
  * @param[in]       params       pointer to model parameter
  * @param[in]       buffers      pointer to buffer spaces
+ * @param[in]       backward     direction of the pass, 0 for forward, 1 for backward
+ * @param[in]       normalize    apply mean-var normalization, 0 for no, 1 for yes
  * @return     The function returns <code>0</code> on success
  *             <code>ERR_PRECOMP_NOT_INIT</code> if preComp not allocated
  *             <code>ERR_NORMFEAT_NOT_INIT</code> if normFeatures not allocated
 */
 int fastgrnn(float* const hiddenState, unsigned hiddenDims,
   const float* const input, unsigned inputDims, unsigned steps,
-  const FastGRNN_Params* params, FastGRNN_Buffers* buffers) {
-
-  if (buffers->preComp == 0) return ERR_PRECOMP_NOT_INIT;
-  if (buffers->normFeatures == 0) return ERR_NORMFEATURES_NOT_INIT;
-
-  for (unsigned t = 0; t < steps; t++) {
-    // Normalize the features
-    v_add(1.0f, input + t * inputDims, -1.0f, params->mean + t * inputDims,
-      inputDims, buffers->normFeatures);
-    v_div(params->stdDev + t * inputDims, buffers->normFeatures, inputDims,
-      buffers->normFeatures);
-
-    // Process the new input and previous hidden state
-    matVec(params->W, buffers->normFeatures, hiddenDims, inputDims, 
-      0.0f, 1.0f, buffers->preComp);
-    matVec(params->U, hiddenState, hiddenDims, hiddenDims, 
-      1.0f, 1.0f, buffers->preComp);
-
-    // Apply the gate to generate the new hidden state
-    for (unsigned i = 0; i < hiddenDims; i++) {
-      float gate = sigmoid(buffers->preComp[i] + params->Bg[i]);
-      float update = tanh(buffers->preComp[i] + params->Bh[i]);
-      hiddenState[i] = gate * hiddenState[i] + (params->zeta * (1.0 - gate) + params->nu) * update;
-    }
-  }
-  return 0;
-}
+  const void* params, void* buffers, int backward, int normalize);
 
 #endif
---
+++ c_reference/include/rnnpool.h
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+#ifndef __RNNPOOL_H__
+#define __RNNPOOL_H__
+
+typedef int (*rnn_t)(float* const, unsigned, const float* const, unsigned, unsigned, const void*, void*, int, int);
+
+/**
+ * @param[in]        patch          pointer to activation of patch (row, col, channel)
+ * @param[in]        inputDims      dimemsion of each input pixel
+ * @param[in]        patchDim       number of rows and columns in a square patch
+ * @param[in]        stride         stride lenghth in the larger image to get to next row
+ * @param[in]        rnn1           function pointer to RNN1
+ * @param[in]        hiddenDims1    dimension of the hidden state of RNN1
+ * @param[in]        rnn1_params    pointer to parameters of RNN1
+ * @param[in]        rnn1_buffers   pointer to buffers needed for RNN1
+ * @param[in]        rnn2           function pointer to RNN2
+ * @param[in]        hiddenDims2    dimension of the hidden state of RNN2
+ * @param[in]        rnn2_params    pointer to parameters of RNN2
+ * @param[in]        rnn2_buffers   pointer to buffers needed for RNN2
+ * @param[out]       output         pointer to output, initialized to size 4 * hiddenDims2
+ * @param[in,out]    buffer         pointer to buffer, intialized to size hiddenDims1 * max{nrows, cols}
+ */
+int rnnpool_block(const float* const patch, unsigned inputDims,
+  unsigned patchDim, unsigned stride,
+  rnn_t rnn1, unsigned hiddenDims1, const void* rnn1_params, void* rnn1_buffers,
+  rnn_t rnn2, unsigned hiddenDims2, const void* rnn2_params, void* rnn2_buffers,
+  float* const output, float* const buffer);
+
+
+#endif
