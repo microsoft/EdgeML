56d979b64c5423bc6d13eb35a7099df1271a2cb6
Arun KV <arun.kv@vvdntech.in>
2020-06-26 16:29:04 +0530

    Remaining functions are converted to C,(maxpool,convolution,conv) Compilation done, No-warning.

--- c_reference/src/quantized_utils.c
+++ c_reference/src/quantized_utils.c
@@ -255,8 +255,10 @@ void transpose(INT_T *A, INT_T *B, INT_T I, INT_T J) {
   }
   return;
 }
-
-void add_or_sub_cir_4D(INT_T *A, const INT_T *B, INT_T *X, INT_T N, INT_T H,     \
+/*
+   For 2D array, give W = 0, C = 0
+*/
+void add_or_sub_cir_4D_2D(INT_T *A, const INT_T *B, INT_T *X, INT_T N, INT_T H,     \
                   INT_T W, INT_T C, INT_T shrA, INT_T shrB, INT_T shrC, uint8_t add) {
   INT_T n     = 0;
   INT_T c     = 0; 
@@ -265,6 +267,13 @@ void add_or_sub_cir_4D(INT_T *A, const INT_T *B, INT_T *X, INT_T N, INT_T H,    
   INT_T res   = 0;
 
   if(A && B && X) {
+
+    if( W == 0 && C == 0 ) {
+
+      W = 1;
+      C = 1;
+    }
+
     for (n = 0; n < N * H * W * C; n++) {
           a = A[n];
 #ifdef SHIFT
@@ -299,62 +308,10 @@ void add_or_sub_cir_4D(INT_T *A, const INT_T *B, INT_T *X, INT_T N, INT_T H,    
 
   return;
 }
-
-
-void add_or_sub_cir_2D(INT_T *A, const INT_T *B, INT_T *X, INT_T H, INT_T W,  \
-                   INT_T shrA, INT_T shrB, INT_T shrC, uint8_t add) { 
-
-  INT_T h   = 0;
-  INT_T w   = 0;
-  INT_T a   = 0;
-  INT_T b   = 0; 
-  INT_T res = 0;
-
-  if(A && B && X) {
-    
-    for (h = 0; h < H * W; h++) {
-        a = A[h];
-#ifdef SHIFT
-        a >>= shrA;
-#else
-        a = a / shrA;
-#endif /* SHIFT */
-
-        b = B[w++];
-        if(w >= W)
-          w = 0;
-#ifdef SHIFT
-        b >>= shrB;
-#else
-        b = b / shrB;
-#endif /* SHIFT */
-
-        if (add) {
-#ifdef SHIFT
-            res = ((a+b) >> shrC);
-#else
-            res = ( (a + b) / shrC);
-#endif /* SHIFT */
-        }
-        else {
-#ifdef SHIFT
-            res = ((a-b) >> shrC);
-#else
-            res = ( (a- b) / shrC);
-#endif /* SHIFT */
-        }
-
-        X[h] = res;
-    }
-  }
-
-  return;
-}
-
 /*
    For 2D array, give W = 0, C = 0
 */
-void relu_4D(INT_T *A, INT_T N, INT_T H, INT_T W, INT_T C) { 
+void relu_4D_2D(INT_T *A, INT_T N, INT_T H, INT_T W, INT_T C) { 
 
   INT_T n = 0;
   
@@ -458,7 +415,7 @@ void adjust_scale_shr(INT_T *A, INT_T I, INT_T J, INT_T K, INT_T L, INT_T scale)
 
   if(A) {
 
-    if(K == 0 && L == 0 ) {
+    if( K == 0 && L == 0 ) {
 
       K = 1;
       L = 1;
@@ -538,4 +495,296 @@ void Reverse2(INT_T *A, INT_T axis, INT_T I, INT_T J, INT_T *B) {
   }
 
   return;
+}
+void maxpool(INT_T *A, INT_T *B, INT_T N, INT_T H, INT_T W, INT_T C, INT_T FH, \
+             INT_T FW, INT_T strideH, INT_T strideW, INT_T HPADL, INT_T HPADR, \
+            INT_T WPADL, INT_T WPADR) {
+
+  INT_T n   = 0;
+  INT_T ho  = 0;
+  INT_T wo  = 0;
+  INT_T c   = 0;
+  INT_T max = 0;
+  INT_T a   = 0;
+  INT_T hs  = 0;
+  INT_T ws  = 0;
+  INT_T HO  = 0;
+  INT_T WO  = 0;
+
+
+#ifdef SHIFT 
+  HO = H >> strideH;
+  WO = W >> strideW;
+#else
+  HO = H / strideH;
+  WO = W / strideW;
+#endif /* SHIFT */
+  if(A && B) {
+    for (n = 0; n < N; n++) {
+      for (ho = 0; ho < HO; ho++) {
+        for (wo = 0; wo < WO; wo++) {
+          for (c = 0; c < C; c++) {
+
+            max = A[n * H * W * C + (strideH * ho) * W * C + (strideW * wo) * C + c];
+            for (hs = 0; hs < FH; hs++) {
+              for (ws = 0; ws < FW; ws++) {
+                a = A[n * H * W * C + ((strideH * ho) + hs) * W * C + ((strideW * wo) + ws) * C + c];
+                if (a > max)
+                  max = a;
+              }
+            }
+
+            B[n * HO * WO * C + ho * WO * C + wo * C + c] = max;
+          }
+        }
+      }
+    }
+  }
+  return;
+}
+
+void conv(INT_T *A, const INT_T *B, INT_T *C, INT_T *tmp,                   \
+          INT_T N, INT_T H, INT_T W, INT_T CI, INT_T HF,                    \
+          INT_T WF, INT_T CO, INT_T shrA, INT_T shrB, INT_T H1, INT_T H2)
+{
+  INT_T     padH      =  0;
+  INT_T     padW      =  0;
+  INT_T     n         =  0;
+  INT_T     h         =  0;
+  INT_T     w         =  0;
+  INT_T     co        =  0;
+  INT_T     counter   =  0;
+  INT_T     hf        =  0;
+  INT_T     wf        =  0;
+  INT_T     ci        =  0;
+  INT_T     a         =  0;
+  INT_T     b         =  0;
+  INT_T     totalEle  =  0;
+  INT_T     count     =  0;
+  INT_T     depth     =  0;
+  INT_T     p         =  0;
+  INT_T     sum       =  0;
+  uint8_t   shr       =  0;
+
+  if(A && B && C && tmp)
+  { 
+    padH      = (HF - 1) / 2;
+    padW      = (WF - 1) / 2;
+
+    for (n = 0; n < N; n++)
+    {
+      for (h = 0; h < H; h++)
+      {
+        for (w = 0; w < W; w++)
+        {
+          for (co = 0; co < CO; co++)
+          {
+
+            counter = 0;
+            for (hf = 0; hf < HF; hf++)
+            {
+              for (wf = 0; wf < WF; wf++)
+              {
+                for (ci = 0; ci < CI; ci++)
+                {
+                  a = (((((h + hf) < padH) || ((h + hf) >= (H + padH))) || (((w + wf) < padW) || ((w + wf) >= (W + padW)))) ? 0 : A[n * H * W * CI + ((h + hf) - padH) * W * CI + ((w + wf) - padW) * CI + ci]);
+                  b = B[hf * WF * CI * CO + wf * CI * CO + ci * CO + co];
+
+#ifdef SHIFT
+                  a = a >> shrA;
+                  b = b >> shrB;
+#else
+                  a = a / shrA;
+                  b = b / shrB;
+#endif
+                  
+                  tmp[counter] = a * b;
+                  counter++;
+                }
+              }
+            }
+
+            totalEle  = HF * WF * CI;
+            count     = HF * WF * CI; 
+            shr       = 1;
+
+            while (depth < (H1 + H2))
+            {
+              if (depth >= H1)
+                shr = 0;
+              
+#ifdef SHIFT
+              for (p = 0; p < ( (totalEle >> 1 ) + 1); p++)
+#else
+              for (p = 0; p < ( (totalEle / 2) + 1); p++)
+#endif
+              {
+                if (p < (count >> 1))
+                {
+                  if (shr)
+#ifdef SHIFT
+                    sum = ( tmp[2 * p] >> 1 ) + ( tmp[(2 * p) + 1] >> 1 );
+#else
+                    sum = tmp[2 * p] / 2 + tmp[(2 * p) + 1] / 2;
+#endif
+                  else
+                    sum = tmp[2 * p] + tmp[(2 * p) + 1];
+                }
+                else if ((p == (count >> 1)) && ((count & 1) == 1))
+                {
+                  if (shr)
+#ifdef SHIFT
+                    sum = tmp[2 * p] >> 1;
+#else
+                    sum = tmp[2 * p] / 2;
+#endif
+                  else
+                    sum = tmp[2 * p];
+                }
+                else
+                  sum = 0;
+
+                tmp[p] = sum;
+              }
+#ifdef SHIFT
+              count = (count + 1) >> 1;
+#else
+              count = (count + 1) / 2;
+#endif
+
+              depth++;
+            }
+
+            C[n * H * W * CO + h * W * CO + w * CO + co] = tmp[0];
+          }
+        }
+      }
+    }
+  }
+
+  return;
+}
+void convolution(INT_T *A, const INT_T *B, INT_T *C, INT_T *tmp, 
+                 INT_T N, INT_T H, INT_T W, INT_T CIN, INT_T HF,        \
+                 INT_T WF, INT_T CINF, INT_T COUTF, INT_T HOUT,         \
+                 INT_T WOUT, INT_T HPADL, INT_T HPADR, INT_T WPADL,     \
+                 INT_T WPADR, INT_T HSTR, INT_T WSTR, INT_T HDL,        \
+                 INT_T WDL, INT_T G, INT_T shrA, INT_T shrB, INT_T H1,  \
+                 INT_T H2) {
+  
+  INT_T     HOffsetL  = 0; 
+  INT_T     WOffsetL  = 0;
+  INT_T     HOffsetR  = 0;
+  INT_T     WOffsetR  = 0;
+  INT_T     n         = 0;
+  INT_T     h         = 0;
+  INT_T     w         = 0;
+  INT_T     g         = 0;
+  INT_T     co        = 0;
+  INT_T     counter   = 0;
+  INT_T     hf        = 0;
+  INT_T     wf        = 0;
+  INT_T     ci        = 0;
+  INT_T     a         = 0;
+  INT_T     b         = 0;
+  INT_T     p         = 0;
+  INT_T     totalEle  = 0;
+  INT_T     count     = 0;
+  INT_T     depth     = 0;
+  INT_T     sum       = 0;
+  INT_T     hout      = 0;
+  INT_T     wout      = 0;
+  uint8_t   shr       = 0;
+
+  if( A && B && C && tmp)
+  {
+    HOffsetL = HDL*(HF/2) - HPADL;
+    WOffsetL = WDL*(WF/2) - WPADL;
+    HOffsetR = HDL*(HF/2) - HPADR;
+    WOffsetR = WDL*(WF/2) - WPADR;
+
+    for(n = 0; n < N; n++) {
+      for(h = HOffsetL, hout = 0; h < H - HOffsetR; h += HSTR, hout++) {
+        for(w = WOffsetL, wout = 0; w < W - WOffsetR; w += WSTR, wout++) {
+          for(g = 0; g < G; g++) {
+            for(co = 0; co < COUTF; co ++) {
+
+              counter = 0;
+              for(hf = -(HF/2); hf <= HF/2; hf++) {
+                for(wf = -(WF/2); wf <= WF/2; wf++) {
+                  for(ci = 0; ci < CINF; ci++) {
+
+                    a = (((h + HDL * hf) < 0) || ((h + HDL * hf) >= H) ||   \
+                         ((w + WDL * wf) < 0) || ((w + WDL * wf) >= W))     \
+                          ? 0 : A[n * H * W * CIN + (h + HDL * hf) * W *    \
+                            CIN + (w + WDL * wf) * CIN + (ci + g * CINF)];
+
+                    b = B[g * HF * WF * CINF * COUTF + (hf + HF/2) * WF *   \
+                        CINF * COUTF + (wf + WF/2) * CINF * COUTF + ci * COUTF + co];
+
+#ifdef SHIFT
+                    a = a >> shrA;
+                    b = b >> shrB;
+#else
+                    a = a / shrA;
+                    b = b / shrB;
+#endif
+
+                    tmp[counter] = a * b;
+                    counter++;
+                  }
+                }
+              }
+
+              totalEle  = HF * WF * CINF;
+              count     = HF * WF * CINF;
+              shr       = 1;
+
+              while (depth < (H1 + H2)) {
+                if (depth >= H1)
+                  shr = 0;
+
+                for (p = 0; p < (totalEle / 2 + 1); p++) {
+                  
+                  if (p < (count >> 1)) {
+                    if (shr)
+#ifdef SHIFT
+                      sum = tmp[2 * p] >> 1 + tmp[(2 * p) + 1] >> 1;
+#else
+                      sum = tmp[2 * p] / 2 + tmp[(2 * p) + 1] / 2;
+#endif
+                    else
+                      sum = tmp[2 * p] + tmp[(2 * p) + 1];
+                  }
+                  else if ((p == (count >> 1)) && ((count & 1) == 1)) {
+                    if (shr)
+#ifdef SHIFT
+                      sum = tmp[2 * p] >> 1;
+#else
+                      sum = tmp[2 * p] / 2;
+#endif
+                    else
+                      sum = tmp[2 * p];
+                  }
+                  else
+                    sum = 0;
+
+                  tmp[p] = sum;
+                }
+#ifdef SHIFT
+                count = (count + 1) >> 1;
+#else
+                count = (count + 1) / 2 ;
+#endif
+                depth++;
+              }
+
+              C[n * HOUT * WOUT * (COUTF * G) + hout * WOUT * (COUTF * G)   \
+                + wout * (COUTF * G) + (co + g * COUTF)] = tmp[0];
+            }
+          }
+        }
+      }
+    }
+  }
 }
