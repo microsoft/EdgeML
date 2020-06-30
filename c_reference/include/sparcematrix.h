ad43ea276d1796f6a2f8a76ab7f7339c831edf79
Arun KV <arun.kv@vvdntech.in>
2020-06-30 18:02:40 +0530

    Review comments updated, added function header with more descriptive, Test cases results compared with c++ code and fixed some issues,Code allignment,Variable names are modified. Compilation done with 0 warnings

---
+++ c_reference/include/sparcematrix.h
@@ -0,0 +1,19 @@
+
+#ifndef __SPARCEMATRIX_H__
+#define __SPARCEMATRIX_H__
+
+/**
+ * @brief 
+ * @param[in]       
+ * @param[in]       
+ * @param[in]       
+ * @param[in]       
+   @return          none
+ * @example         
+ *                            
+ *                            
+ *                  
+*/
+void sp_mat_mul(const INT_T *Aidx, const INT_T *Aval, INT_T **B, INT_T *C, INT_T K, \
+                INT_T shrA, INT_T shrB, INT_T shrC);
+#endif
