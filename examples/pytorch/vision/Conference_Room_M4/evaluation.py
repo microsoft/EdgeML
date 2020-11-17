58e4b106c0bea3ad0bc125ba2fd68f36da22b93d
Oindrila Saha <t-oisaha@microsoft.com>
2020-11-18 02:48:17 +0530

    evaluation code

---
+++ examples/pytorch/vision/Conference_Room_M4/evaluation.py
@@ -0,0 +1,211 @@
+"""
+WiderFace evaluation code
+author: wondervictor
+mail: tianhengcheng@gmail.com
+copyright@wondervictor
+"""
+
+import os
+import tqdm
+import pickle
+import argparse
+import numpy as np
+from scipy.io import loadmat
+from bbox import bbox_overlaps
+from IPython import embed
+import cv2
+
+
+def read_pred_file(filepath):
+
+    with open(filepath, 'r') as f:
+        lines = f.readlines()
+        img_file = lines[0].rstrip('\n\r')
+        lines = lines[2:]
+
+    boxes = np.array(list(map(lambda x: [float(a) for a in x.rstrip('\r\n').split(' ')], lines))).astype('float')
+    return img_file.split('/')[-1], boxes
+
+
+def get_preds(pred_dir):
+    events = os.listdir(pred_dir)
+    boxes = dict()
+    pbar = tqdm.tqdm(events)
+
+    for event in pbar:
+        pbar.set_description('Reading Predictions ')
+        event_dir = os.path.join(pred_dir, event)
+        event_images = os.listdir(event_dir)
+        current_event = dict()
+        for imgtxt in event_images:
+            imgname, _boxes = read_pred_file(os.path.join(event_dir, imgtxt))
+            current_event[imgname.rstrip('.jpg')] = _boxes
+        boxes[event] = current_event
+    return boxes
+
+
+def norm_score(pred):
+    """ norm score
+    pred {key: [[x1,y1,x2,y2,s]]}
+    """
+
+    max_score = 0
+    min_score = 1
+
+    for _, k in pred.items():
+        for _, v in k.items():
+            if len(v) == 0:
+                continue
+            _min = np.min(v[:, -1])
+            _max = np.max(v[:, -1])
+            max_score = max(_max, max_score)
+            min_score = min(_min, min_score)
+
+    diff = max_score - min_score
+    for _, k in pred.items():
+        for _, v in k.items():
+            if len(v) == 0:
+                continue
+            v[:, -1] = (v[:, -1] - min_score)/diff
+
+
+def image_eval(pred, gt, iou_thresh):
+    """ single image evaluation
+    pred: Nx5
+    gt: Nx4
+    ignore:
+    """
+
+    _pred = pred.copy()
+    _gt = gt.copy()
+    pred_recall = np.zeros(_pred.shape[0])
+    recall_list = np.zeros(_gt.shape[0])
+    proposal_list = np.ones(_pred.shape[0])
+
+    _pred[:, 2] = _pred[:, 2] + _pred[:, 0]
+    _pred[:, 3] = _pred[:, 3] + _pred[:, 1]
+    _gt[:, 2] = _gt[:, 2] + _gt[:, 0]
+    _gt[:, 3] = _gt[:, 3] + _gt[:, 1]
+
+    overlaps = bbox_overlaps(_pred[:, :4], _gt)
+
+    for h in range(_pred.shape[0]):
+
+        gt_overlap = overlaps[h]
+        max_overlap, max_idx = gt_overlap.max(), gt_overlap.argmax()
+        if max_overlap >= iou_thresh:
+            if recall_list[max_idx] == 0:
+                recall_list[max_idx] = 1
+
+        r_keep_index = np.where(recall_list == 1)[0]
+        pred_recall[h] = len(r_keep_index)
+    return pred_recall, proposal_list
+
+
+def img_pr_info(thresh_num, pred_info, proposal_list, pred_recall):
+    pr_info = np.zeros((thresh_num, 2)).astype('float')
+    for t in range(thresh_num):
+
+        thresh = 1 - (t+1)/thresh_num
+        r_index = np.where(pred_info[:, 4] >= thresh)[0]
+        if len(r_index) == 0:
+            pr_info[t, 0] = 0
+            pr_info[t, 1] = 0
+        else:
+            r_index = r_index[-1]
+            p_index = np.where(proposal_list[:r_index+1] == 1)[0]
+            pr_info[t, 0] = len(p_index)
+            pr_info[t, 1] = pred_recall[r_index]
+    return pr_info
+
+
+def dataset_pr_info(thresh_num, pr_curve, count_face):
+    _pr_curve = np.zeros((thresh_num, 2))
+    for i in range(thresh_num):
+        _pr_curve[i, 0] = pr_curve[i, 1] / pr_curve[i, 0]
+        _pr_curve[i, 1] = pr_curve[i, 1] / count_face
+    return _pr_curve
+
+
+def voc_ap(rec, prec):
+
+    # correct AP calculation
+    # first append sentinel values at the end
+    mrec = np.concatenate(([0.], rec, [1.]))
+    mpre = np.concatenate(([0.], prec, [0.]))
+
+    # compute the precision envelope
+    for i in range(mpre.size - 1, 0, -1):
+        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+    # to calculate area under PR curve, look for points
+    # where X axis (recall) changes value
+    i = np.where(mrec[1:] != mrec[:-1])[0]
+
+    # and sum (\Delta recall) * prec
+    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
+
+
+def evaluation(pred, iou_thresh=0.5):
+    f = open('../../data/face_val_scutB.txt')
+    lines = f.readlines()
+    names = []
+    gt_list = []
+    for line in lines:
+        line = line.strip().split()
+        names.append(os.path.split(line[0])[1][0:-4])
+        th, tw, tc = cv2.imread(line[0]).shape
+        num_faces = int(line[1])
+        faces = []
+        for i in range(num_faces):
+            x = float(line[2 + 5 * i])
+            y = float(line[3 + 5 * i])
+            w = float(line[4 + 5 * i])
+            h = float(line[5 + 5 * i])
+
+            faces.append([x, y, w, h])
+        gt_list.append(faces)
+
+    thresh_num = 1000
+
+    aps = []
+
+    count_face = 0
+    pr_curve = np.zeros((thresh_num, 2)).astype('float')
+
+    for j in range(len(names)):
+        gt_boxes = np.asarray(gt_list[j]).astype('float')
+        pred_file = open(os.path.join(pred, names[j] + '.txt'))
+        pred_lines = pred_file.readlines()
+        pred_info = np.array(list(map(lambda x: [float(a) for a in x.rstrip('\r\n').split(' ')], pred_lines[2:]))).astype('float')
+    
+
+        count_face += len(gt_boxes)
+
+        if len(gt_boxes) == 0 or len(pred_info) == 0:
+            continue
+
+        pred_recall, proposal_list = image_eval(pred_info, gt_boxes, iou_thresh)
+
+        _img_pr_info = img_pr_info(thresh_num, pred_info, proposal_list, pred_recall)
+
+        pr_curve += _img_pr_info
+    pr_curve = dataset_pr_info(thresh_num, pr_curve, count_face)
+
+    propose = pr_curve[:, 0]
+    recall = pr_curve[:, 1]
+
+    ap = voc_ap(recall, propose)
+    aps.append(ap)
+    print(ap)
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--pred')
+
+    args = parser.parse_args()
+    evaluation(args.pred)
